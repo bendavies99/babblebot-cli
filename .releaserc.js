48e46487a955a7e54bf1cfa9f04126be713d45c8
Ben Davies <me@bdavies.net>
2021-01-07 12:18:58 +0000

    build: add Github to release list

--- .releaserc.js
+++ .releaserc.js
@@ -5,6 +5,7 @@ module.exports = {
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         ['@semantic-release/git', { assets: ["CHANGELOG.md"] }],
+        '@semantic-release/github',
         '@semantic-release/npm',
     ],
 };
